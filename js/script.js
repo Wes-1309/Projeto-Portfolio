99701e7bfd7917b5970ebbaf3756c97cb4cb1c54
Wesley Coelho <wes.coelho@yahoo.com.br>
2021-07-15 20:44:58 -0300

    Atualização HTML, CSS e JS

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Função para o menu de abrir e fechar
+
+document.querySelector(".hamburguer");addEventListener("click", () => 
+    document.querySelector(".container").classList.toggle("show-menu")
+);
